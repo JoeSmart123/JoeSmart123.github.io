bf3a146575a25e4c8ce49cb27cc8c389caa39448
JoeSmart123 <joseph2008smart@gmail.com>
2026-03-19 02:29:52 -0400

    added timer

--- js/script.js
+++ js/script.js
@@ -1,8 +1,35 @@
+let timeLeft = 10;
+let countdow;
+let readyToReset = false;
+
 function sayHello() {
     alert("Hello World!");
 }
 
 function makeGreen() {
-    document.getElementById("box").classList.add("green");
-    alert("updated");
+    if (document.getElementById("box").style.backgroundColor === "green") {
+        document.getElementById("box").style.backgroundColor = "red";
+        console.log("daswdwd");
+    } else {
+        document.getElementById("box").style.backgroundColor = "green";
+    }
+}
+
+function startTimer() {
+    if (!readyToReset) {
+        countdown = setInterval(function () {
+            timeLeft--;
+            document.getElementById("timer").textContent = timeLeft;
+
+            if (timeLeft <= 0) {
+                clearInterval(countdown);
+                alert("times up");
+            }
+
+        }, 1000);
+        timeLeft = 10;
+        readyToReset = true;
+    } else {
+        alert("Do not attempt to beat the counter.")
+    }
 }
