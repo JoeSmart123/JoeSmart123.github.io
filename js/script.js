5a5b1ae94197d97c5358b354ff89d883012d57b7
JoeSmart123 <joseph2008smart@gmail.com>
2026-03-20 14:13:13 -0400

    added teams selection

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@ let timeLeft = 10;
 let countdow;
 let readyToReset = false;
 let currentWord = "Please Generate"
+let teams = [];
+
 
 function sayHello() {
     alert("Hello World!");
@@ -42,4 +44,65 @@ function init() {
 function generate() {
     makeGreen();
     startTimer();
+}
+
+// function createTeam(teamName) {
+//     localStorage.setItem('team', JSON.stringify(teamName))
+// }
+
+function createTeam(name1, name2) {
+    teams.push({name1, name2, score: 0});
+    renderTeams();
+}
+
+function addPoint(index) {
+    teams[index].score++;
+    renderTeams();
+}
+
+function removePoint(index) {
+    if (teams[index].score <= 0) {
+        alert("No points to remove");
+    } else {
+        teams[index].score--;
+        renderTeams();
+    }
+}
+
+function removeTeam(index) {
+    if (confirm("Are you sure you want to remove this team?")) {
+        teams.splice(index, 1);
+        renderTeams();    
+    }
+    
+}
+
+function renderTeams() {
+    const container = document.getElementById("teamsContainer");
+    container.innerHTML = "";
+
+    teams.forEach((team, index) => {
+        container.innerHTML += `
+            <div class="team">
+                <p>${team.name1} & ${team.name2}</p>
+                <p>Score: ${team.score}</p>
+                <div class="teamButtons">
+                    <button id="teamBox" onclick="addPoint(${index})">+1</button>
+                    <button id="teamBox" onclick="removeTeam(${index})">RemoveTeam</button>
+                    <button id="teamBox" onclick="removePoint(${index})">-1</button>
+                </div>
+            </div>
+        `;
+    });
+}
+
+function addTeam() {
+    const name1 = document.getElementById("p1").value;
+    const name2 = document.getElementById("p2").value;
+
+    createTeam(name1, name2);
+}
+
+function goToScoreboard() {
+    window.location.href = "scoreboard.html";
 }
